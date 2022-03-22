be53ff81d927d0a9305d6703783a6d406dd1ab31
Chua Jia Zhe <jia.zheeeee@gmail.com>
2022-03-22 13:12:44 +0800

    Smali: Renaming, fixing bugs.

--- obfuscate_smali_field_renaming.py
+++ obfuscate_smali_field_renaming.py
@@ -1,10 +1,12 @@
 import util
 
+
 def rename_field(field_name):
     field_md5 = util.get_string_md5(field_name)
     return "f{0}".format(field_md5.lower()[:8])
 
-def get_sdk_class_names(smali_files):
+
+def get_class_names(smali_files):
     class_names = set()
     for smali_file in smali_files:
         with open(smali_file, "r", encoding="utf-8") as current_file:
@@ -19,11 +21,13 @@ def get_sdk_class_names(smali_files):
                     break
     return class_names
 
+
 def rename_field_declarations(smali_files):
     renamed_fields = set()
 
     # Search for field definitions that can be renamed.
-    for smali_file in util.show_list_progress(smali_files, interactive=False,description="Renaming field declarations"):
+    for smali_file in util.show_list_progress(smali_files, interactive=False,
+                                              description="Renaming field declarations"):
         with util.inplace_edit_file(smali_file) as (in_file, out_file):
             class_name = None
             for line in in_file:
@@ -52,7 +56,6 @@ def rename_field_declarations(smali_files):
                         )
                         out_file.write(line)
 
-
                         for _ in range(util.get_random_int(1, 4)):
                             out_file.write("\n")
                             out_file.write(
@@ -71,8 +74,8 @@ def rename_field_declarations(smali_files):
                         out_file.write(line)
                 else:
                     out_file.write(line)
-
     return renamed_fields
+
 
 def rename_field_references(fields_to_rename, smali_files, sdk_classes):
     count = 0
@@ -95,7 +98,7 @@ def rename_field_references(fields_to_rename, smali_files, sdk_classes):
                         # Rename field usage.
                         count += 1
                         out_file.write(
-                            line.replace("{0}:".format(field_name),"{0}:".format(rename_field(field_name)))
+                            line.replace("{0}:".format(field_name), "{0}:".format(rename_field(field_name)))
                         )
                     else:
                         out_file.write(line)
@@ -103,35 +106,12 @@ def rename_field_references(fields_to_rename, smali_files, sdk_classes):
                     out_file.write(line)
     return count
 
+
 def rename(smali_files):
     try:
-        sdk_class_declarations = get_sdk_class_names(smali_files)
-        renamed_field_declarations = set()
-
-
-
-        # if obfuscation_info.is_multidex():
-        #     for index, dex_smali_files in enumerate(util.show_list_progress(
-        #                 obfuscation_info.get_multidex_smali_files(),
-        #                 interactive=False,
-        #                 unit="dex",
-        #                 description="Processing multidex",
-        #             )
-        #     ):
-        #         self.max_fields_to_add = (obfuscation_info.get_remaining_fields_per_obfuscator()[index])
-        #         added_fields = 0
-        #         renamed_field_declarations.update(rename_field_declarations(dex_smali_files))
-        # else:
+        sdk_class_declarations = get_class_names(smali_files)
         renamed_field_declarations = rename_field_declarations(smali_files)
-
-        # When renaming field references it makes no difference if this is a
-        # multidex application, since at this point we are not introducing any new
-        # field.
         count = rename_field_references(renamed_field_declarations, smali_files, sdk_class_declarations)
         return count
-
-
     except:
         print("Something went wrong with renaming.")
-
-
