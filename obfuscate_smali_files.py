aa9b918b9670795eec7c047a71cb7398f67de895
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2022-03-29 19:07:15 +0800

    added file removal for obfuscate_smali_files.py

--- obfuscate_smali_files.py
+++ obfuscate_smali_files.py
@@ -4,28 +4,34 @@ import glob
 
 
 def backup_files(list_of_files):
+    print("Here")
     backup_path = os.getcwd() + '\\diffviewer\\bak\\'
-    files = glob.glob(backup_path)
-    if len(files) > 1:
-        for f in files:
+    files = glob.glob(backup_path + '/*')
+    for f in files:
+        try:
             os.remove(f)
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
 
     for file in list_of_files:
         src = str(file)
         dst = backup_path + str(file.split('\\')[-1])
-        # print(src, "<====> ", dst)
+        print(src, "<====> ", dst)
         shutil.copy(src, dst, follow_symlinks=False)
 
 
 def generate_new_files(list_of_files):
     new_path = os.getcwd() + '\\diffviewer\\new\\'
-    files = glob.glob(new_path)
-    if len(files) > 1:
-        for f in files:
+    files = glob.glob(new_path + '/*')
+    for f in files:
+        print(f)
+        try:
             os.remove(f)
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
 
     for file in list_of_files:
         src = str(file)
         dst = new_path + str(file.split('\\')[-1])
-        # print(src, "<====> ", dst)
+        print(src, "<====> ", dst)
         shutil.copy(src, dst, follow_symlinks=False)
