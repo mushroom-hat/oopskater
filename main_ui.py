53d2da7a51bb89d4ccf12e33d5bd01337592cf15
Chua Jia Zhe <jia.zheeeee@gmail.com>
2022-03-30 12:28:28 +0800

    Update UI

--- main_ui.py
+++ main_ui.py
@@ -201,7 +201,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Android / JAVA Obfuscator"))
         self.importFileButton.setText(_translate("MainWindow", "Add File / Directory"))
         self.input_apk.setText(_translate("MainWindow", "Not File / Directory Selected."))
         self.input_keystore.setText(
