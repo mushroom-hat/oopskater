c93530f93bdf8d79e38d2e29d6de21c4a8703888
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2022-04-03 15:52:58 +0800

    Update obfuscate_smali_files.py

--- obfuscate_smali_files.py
+++ obfuscate_smali_files.py
@@ -5,6 +5,7 @@ import glob
 
 def backup_files(list_of_files):
     backup_path = os.getcwd() + '\\diffviewer\\bak\\'
+    new_path = os.getcwd() + '\\diffviewer\\new\\'
 
     files = glob.glob(backup_path + '/*')
     for f in files:
@@ -13,6 +14,12 @@ def backup_files(list_of_files):
         except OSError as e:
             print("Error: %s : %s" % (f, e.strerror))
 
+    files = glob.glob(new_path + '/*')
+    for f in files:
+        try:
+            os.remove(f)
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
     for file in list_of_files:
         src = str(file)
         dst = backup_path + str(file.split('\\')[-1])
@@ -21,6 +28,7 @@ def backup_files(list_of_files):
 
 def generate_new_files(list_of_files):
     new_path = os.getcwd() + '\\diffviewer\\new\\'
+
     files = glob.glob(new_path + '/*')
     for f in files:
         try:
